bcc0fee8d3f27083650687dbea7720c23179e441
bmilne64 <bronwynmilne64@gmail.com>
2022-11-21 21:38:22 -0500

    edits app.py

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/precipitation<br/>Returns a list of the prior year's precipitation data for all stations.<br/>"
+        f"/api/v1.0/stations<br/>Returns a list of all station numbers and names.<br/>"
+        f"/api/v1.0/tobs<br/>Returns a list of the prior year's min, max and avg temperatures from all stations.<br/>"
+        f"/api/v1.0/<start><br/>Input a start date (YYYY-MM-DD) to calculate the min, max and avg temperature for all dates greater than or equal to the start date.<br/>"
+        f"/api/v1.0/<start>/<end><br/>Input a start and end date (YYYY-MM-DD) to calculate the min, max and avg temperature for dates between the start and end date.<br/>"
     )
 
 #precipitation route
@@ -57,7 +57,7 @@ def precipitation():
     last_year = dt.date(2017, 8 ,23) - dt.timedelta(days=365)
     
     # Query all precipitation
-    scores = session.query(Measurement.date, Measurement.prcp).filter((Measurement.date > last_year) & (Measurement.date < most_recent))
+    scores = session.query(Measurement.date, Measurement.prcp).filter((Measurement.date >= last_year) & (Measurement.date <= most_recent))
 
     session.close()
 
@@ -81,37 +81,75 @@ def stations():
     # Starting from the most recent data point in the database. 
     stations = session.query(Station.station, Station.name).all()
     
+    stations_list = list(np.ravel(stations))
+    
     session.close()
 
-    return jsonify(stations)  
+    return jsonify(stations_list)  
     
-# #tobs api
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+#tobs api
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     
-#     """Return a list of all tobs for most active station"""
-#     # Starting from the most recent data point in the database. 
-#     most_recent_date = list(session.query(Measurement.date).order_by(Measurement.date.desc()).first())
-#     most_recent = most_recent_date[0]
+    """Return a list of all tobs for most active station"""
 
-#     # Calculate the date one year from the last date in data set.
-#     last_year = dt.date(2017, 8 ,23) - dt.timedelta(days=365)
+    most_recent_date = list(session.query(Measurement.date).order_by(Measurement.date.desc()).first())
+    most_recent = most_recent_date[0]
+    last_year = dt.date(2017, 8 ,23) - dt.timedelta(days=365)
     
-#     # Starting from the most recent data point in the database. 
-#     station_7 = (session.query(Measurement.tobs).filter((Measurement.date > last_year) & (Measurement.date < most_recent(Station.id == 7) & (Measurement.station == Station.station)
-    
-#     session.close()
-                                        
-#     return jsonify(station_7)    
+    station_7 = list(session.query(Measurement.tobs)
+              .filter((Measurement.date >= last_year) & 
+                      (Measurement.date <= most_recent) & 
+                      (Station.id == 7) & 
+                      (Measurement.station == Station.station)))
+                 
+    session.close() 
+                                                        
+    station7_list = list(np.ravel(station_7))                
+    return jsonify(station7_list)    
     
 @app.route("/api/v1.0/<start>")
-def start(start):
+@app.route("/api/v1.0/<start>/<end>")
+
+def start(start = None, end = None):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    measurements = [func.max(Measurement.tobs),func.min(Measurement.tobs),func.avg(Measurement.tobs)]
+    
+    if not end:
+    
+        start= dt.datetime.strptime(start, '%Y-%m-%d')
+                                     
+        start_only = session.query(*measurements).filter(Measurement.date >= start).all()
+    
+        session.close() 
+    
+        start_list = list(np.ravel(start_only))  
+    
+        return jsonify(start_list)  
+    
+    start= dt.datetime.strptime(start, '%Y-%m-%d')
+    
+    end = dt.datetime.strptime(end, '%Y-%m-%d')
+    
+    start_end = session.query(*measurements).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    session.close() 
+    
+    end_list = list(np.ravel(start_end))  
+    
+    return jsonify(end_list)  
+    
+    
+                                                                                          
+@app.route("/api/v1.0/<start>/<end>")
+def startend(start, end):
     # Create our session (link) from Python to the DB
     session = Session(engine)
                                         
-    start_only = session.query(func.max(Measurement.tobs),func.min(Measurement.tobs),func.avg(Measurement.tobs)).filter(Station.id == 7).filter(Measurement.station == Station.station).filter(Measurement.date >= start).all()
- 
+    start_end = session.query(func.max(Measurement.tobs),func.min(Measurement.tobs),func.avg(Measurement.tobs)).filter(Station.id == 7).filter(Measurement.station == Station.station).filter(Measurement.date >= start).filter(Measurement.date <= end).all()            
 if __name__ == '__main__':
         app.run(debug=True)
